b3f026b32f7e02155585136e9f7afc133bd9aa6d
Tony <arabyi@yandex.ru>
2020-08-04 15:11:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 class config(object):
-    top = 18 
-    right = 12
-    bottom = 18
-    left = 12
+    top = 18    # diods count on the top
+    right = 12  # diods count on the right
+    bottom = 18 # diods count on the bottom
+    left = 12   # diods count on the left
     
-    height = 10
+    height = 10 # diods count on the top
     width = 10
     
-    offsetTop = 0
-    offsetRight = 0
-    offsetBottom = offsetTop
-    offsetLeft = offsetRight
+    offsetTop = 0               # offset from Top to grab right and left parts of screen
+    offsetBottom = offsetTop    # offset from Bottom to grab right and left parts of screen
     
-    step = 2
+    offsetRight = 0             # offset from Right to grab top and bottom parts of screen
+    offsetLeft = offsetRight    # offset from Left to grab top and bottom parts of screen
+    
+    step = 2    # uses for Retina display, whoose pixel density is 2x more
